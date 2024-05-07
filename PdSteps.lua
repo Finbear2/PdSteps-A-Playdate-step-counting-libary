853c96409d1fe6f30c15c2c1abe8e10dfb1256d8
Finbear2 <168268485+Finbear2@users.noreply.github.com>
2024-05-07 21:17:13 +0100

    Create PdSteps.lua

---
+++ PdSteps.lua
@@ -0,0 +1,112 @@
+import "CoreLibs/timer"
+
+-- Hi, thanks for using my Step Counting Libary!
+-- I hope you make some cool and uniqe games.
+-- From Finley :)
+
+local pd = playdate
+
+
+StepChecking = false
+
+local Pre_Acel
+local Acel
+
+
+PdSteps = {}
+
+Steps = 0
+
+
+function PdSteps:GetSteps()
+    
+    return Steps
+
+end
+
+function PdSteps:GetAreStepsTracking()
+    
+    return StepChecking
+
+end
+
+function PdSteps:ResetSteps()
+    
+    Steps = 0
+
+end
+
+function PdSteps:StartStepChecking( Delay )
+    
+    pd.timer.new( Delay, function ()
+        
+        Pre_Acel = 0
+
+        CheckSteps()
+
+    end)
+
+end
+
+function CheckSteps()
+    
+	pd.startAccelerometer()
+	pd.setAutoLockDisabled( true )
+
+
+	local x, y, z = pd.readAccelerometer()
+
+	Acel = x
+
+	local accellaration
+
+	if Acel > Pre_Acel then
+		
+		accellaration = Acel - Pre_Acel
+
+	else
+
+		accellaration = Pre_Acel - Acel
+
+	end
+
+	if accellaration > 0.9 then
+		
+		Steps += 1
+
+	end
+
+	Pre_Acel = x
+
+	if StepChecking == true then
+
+		pd.timer.new( 23, function ()
+
+			CheckSteps()
+
+		end )
+
+	end
+
+end
+
+function PdSteps:StopStepChecking()
+    
+    StepChecking = false
+
+    pd.stopAccelerometer()
+    pd.setAutoLockDisabled(false)
+
+end
+
+function PdSteps:IncreaseSteps( Amount )
+    
+    Steps += Amount
+
+end
+
+function PdSteps:DecreaseSteps( Amount )
+    
+    Steps -= Amount
+
+end
